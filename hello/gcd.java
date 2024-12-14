cd12da6b8fca3f93c49bde55a1b183b8880b51d6
misssleepyhead <arielchen8549@gmail.com>
2024-12-14 17:36:36 +0800

    book 1.1 example

---
+++ hello/gcd.java
@@ -0,0 +1,21 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class gcd {
+
+    /** Euclid's algorithm */
+    public static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        int r = p % q;
+        return gcd(q, r);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
