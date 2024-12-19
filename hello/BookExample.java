4c7f57b137e0c1ca5d59e2aeb8673b1147f94e8b
misssleepyhead <arielchen8549@gmail.com>
2024-12-19 18:54:57 +0800

    L1 hw

--- hello/BookExample.java
+++ hello/BookExample.java
@@ -4,6 +4,8 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdDraw;
+
 public class BookExample {
 
     /** Euclid's algorithm */
@@ -40,7 +42,20 @@ public class BookExample {
         return t;
     }
 
+    public static void plotFunction() {
+        int N = 100;
+        StdDraw.setXscale(0, N);
+        StdDraw.setYscale(0, N * N);
+        StdDraw.setPenRadius(.01);
+        for (int i = 1; i <= N; i++) {
+            StdDraw.point(i, i);
+            StdDraw.point(i, i * i);
+            StdDraw.point(i, i * Math.log(i));
+        }
+    }
+
     public static void main(String[] args) {
+        plotFunction();
 
     }
 }
