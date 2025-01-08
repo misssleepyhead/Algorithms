222c4a9ee9f616f3ea643e7b6bfc2ba8e4b6ea55
misssleepyhead <arielchen8549@gmail.com>
2025-01-08 15:19:20 +0800

    1.5 QF client test

--- Lecture2/QuickFindUF.java
+++ Lecture2/QuickFindUF.java
@@ -36,4 +36,18 @@ public class QuickFindUF {
     public int count(){
         return id.length;
     }
+
+    public static void main(String[] args) {
+        QuickFindUF q = new QuickFindUF(10);
+        q.union(9,0);
+        q.union(3,4);
+        q.union(5,8);
+        q.union(7,2);
+        q.union(2,1);
+        q.union(5,7);
+        q.union(0,3);
+        q.union(4,2);
+        System.out.println(q.connected(9,2)); // should be true
+        System.out.println(q.connected(3,9));
+    }
 }
