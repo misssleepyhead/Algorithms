14c89a2973006c57e086af370b7074cd33de43db
misssleepyhead <arielchen8549@gmail.com>
2025-01-09 20:15:04 +0800

    Lecture 2 assignment

---
+++ Lecture2/Percolation.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+/** Assignment*/
+
+public class Percolation {
+    private int N ; // N * N grid
+    private boolean[][] grid;
+    private final int VIRTUAL_TOP;
+    private final int VIRTUAL_BOTTOM;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n){}
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col){}
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col){}
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col){}
+
+    // returns the number of open sites
+    public int numberOfOpenSites(){}
+
+    // does the system percolate?
+    public boolean percolates(){}
+
+    // test client (optional){}
+    public static void main(String[] args){}
+}
