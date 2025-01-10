9a23b5c39abb51fc017c800137bdc36f6479635c
misssleepyhead <arielchen8549@gmail.com>
2025-01-10 16:54:04 +0800

    Lecture 2 PercolationStats class

--- Lecture2/PercolationStats.java
+++ Lecture2/PercolationStats.java
@@ -1,23 +1,74 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
-/** Monte Carlo simulation estimate percolation */
+/**
+ * Monte Carlo simulation estimate percolation
+ */
 public class PercolationStats {
+    private int trials;
+    private int totalSites;
+    private final int side;
+    private final double[] thresholds; // Array to store percolation thresholds for each trial
 
+    private double mean;
+    private double stddev;
+    private double confidenceLo;
+    private double confidenceHi;
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials){}
+    public PercolationStats(int n, int trials) {
+        if(n<=0 || trials<=0){
+            throw new IllegalArgumentException("Grid side and trials cannot be 0");
+        }
+        this.side=n;
+        this.trials = trials;
+        this.totalSites = n * n;
+        this.thresholds = new double[trials];
+        this.mean=0;
+        this.stddev=0;
+        this.confidenceLo=0;
+        this.confidenceHi=0;
+    }
+
+    private void simulation(){
+
+        for(int i=0;i<trials;i++){
+            Percolation perc = new Percolation(side);
+            while(!perc.percolates()){
+                int row = StdRandom.uniformInt(1,side+1);
+                int col = StdRandom.uniformInt(1,side+1);
+                while (!perc.isOpen(row,col)) {
+                    perc.open(row, col);
+                }
+            }
+            thresholds[i]=(double) perc.numberOfOpenSites()/totalSites;
+        }
+        this.mean = mean();
+        this.stddev=stddev();
+    }
 
     // sample mean of percolation threshold
-    public double mean(){}
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
 
     // sample standard deviation of percolation threshold
-    public double stddev(){}
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo(){}
+    public double confidenceLo() {
+        return mean - (1.96*stddev)/Math.sqrt(trials);
+    }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){}
+    public double confidenceHi() {
+        return mean + (1.96*stddev)/Math.sqrt(trials);
+    }
 
     // test client (see below)
-    public static void main(String[] args){}
+    public static void main(String[] args) {
+    }
 
 }
