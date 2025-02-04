b2361bc191a7c95374f3de9f2e37a9bcc3d42d9c
misssleepyhead <arielchen8549@gmail.com>
2025-02-04 18:08:28 +0800

    Book 1.3 example Evaluate

--- Lecture4/Evaluate.java
+++ Lecture4/Evaluate.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Stack;
 
@@ -9,45 +10,30 @@ import java.util.Stack;
 public class Evaluate {
 
     public static void main(String[] args) {
-        Stack<String> ops = new Stack<String>();
+        Stack<String> ops  = new Stack<String>();
         Stack<Double> vals = new Stack<Double>();
-        while (!StdIn.isEmpty()) {
-            // Read token, push if operator
-            String s = StdIn.readString();
 
-            if (s.equals("(")){}
-            else if (s.equals("+")) {
-                ops.push(s);
-            } else if (s.equals("-")) {
-                ops.push(s);
-            } else if (s.equals("*")) {
-                ops.push(s);
-            } else if (s.equals("/")) {
-                ops.push(s);
-            } else if (s.equals("sqrt")) {
-                ops.push(s);
-            } else if (s.equals(")")) {
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if      (s.equals("("))               ;
+            else if (s.equals("+"))    ops.push(s);
+            else if (s.equals("-"))    ops.push(s);
+            else if (s.equals("*"))    ops.push(s);
+            else if (s.equals("/"))    ops.push(s);
+            else if (s.equals("sqrt")) ops.push(s);
+            else if (s.equals(")")) {
                 String op = ops.pop();
                 double v = vals.pop();
-                if (op.equals("+")) {
-                    v = vals.pop() + v;
-                } else if (op.equals("-")) {
-                    v = vals.pop() - v;
-                } else if (op.equals("*")) {
-                    v = vals.pop() * v;
-                } else if (op.equals("/")) {
-                    v = vals.pop() / v;
-                } else if (op.equals("sqrt")) {
-                    v = Math.sqrt(v);
-                }
+                if      (op.equals("+"))    v = vals.pop() + v;
+                else if (op.equals("-"))    v = vals.pop() - v;
+                else if (op.equals("*"))    v = vals.pop() * v;
+                else if (op.equals("/"))    v = vals.pop() / v;
+                else if (op.equals("sqrt")) v = Math.sqrt(v);
                 vals.push(v);
-            }else{
-                vals.push(Double.parseDouble(s));
             }
-
+            else vals.push(Double.parseDouble(s));
         }
-        System.out.println(vals.pop());
-
+        StdOut.println(vals.pop());
     }
 }
 
