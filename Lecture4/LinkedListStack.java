5452c4bd75aa23f7e13f1a365eb2dba073f851ac
misssleepyhead <arielchen8549@gmail.com>
2025-02-07 15:07:18 +0800

    Book 1.3 example LinkedListStack

---
+++ Lecture4/LinkedListStack.java
@@ -0,0 +1,117 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * The {@code LinkedListStack} class implement linked list
+ */
+public class LinkedListStack<Item> implements Iterable<Item> {
+    private Node<Item> first;
+    private int n;
+
+
+    private static class Node<Item> {
+        Item item;
+        Node next;
+    }
+
+    /**
+     * Initializes an empty stack
+     */
+    public LinkedListStack() {
+        first = null;
+        n = 0;
+    }
+
+    /**
+     * Returns true if this stack is empty
+     *
+     * @return true of this stack is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * @return the number of items
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Adds the item to this stack
+     *
+     * @param item the item to add
+     */
+    public void push(Item item) {
+        Node<Item> oldfirst = first;
+        first = new Node<Item>();
+
+        first.item = item;
+        first.next = oldfirst;
+        n++;
+    }
+
+    /**
+     * Removes and returns the item most recently added to this stack.
+     *
+     * @return the item most recently added to this stack
+     * @throws java.util.NoSuchElementException if this stack is empty
+     */
+    public Item pop() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        Item item = first.item;
+        first = first.next;
+        n--;
+        return item;
+    }
+
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new LinkedIterator(first);
+    }
+
+    private class LinkedIterator implements Iterator<Item> {
+        private Node<Item> currrent;
+
+        public LinkedIterator(Node<Item> first) {
+            this.currrent = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currrent != null;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = currrent.item;
+            currrent = currrent.next;
+            return item;
+
+        }
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack<String> stack = new LinkedListStack<String>();
+        stack.push("a");
+        stack.push("b");
+        System.out.println(stack.size());//2
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+    }
+}
