2a812a2baa2b0f2a0117fb57f0d369f457843590
misssleepyhead <arielchen8549@gmail.com>
2025-02-07 17:20:13 +0800

    Book 1.3.4 exercises

---
+++ Lecture4/Parentheses.java
@@ -0,0 +1,45 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Scanner;
+
+public class Parentheses {
+    private static final char LEFT_PAREN     = '(';
+    private static final char RIGHT_PAREN    = ')';
+    private static final char LEFT_BRACE     = '{';
+    private static final char RIGHT_BRACE    = '}';
+    private static final char LEFT_BRACKET   = '[';
+    private static final char RIGHT_BRACKET  = ']';
+
+    public static boolean isBalanced(String s) {
+        LinkedListStack<Character> stack = new LinkedListStack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == LEFT_PAREN)   stack.push(LEFT_PAREN);
+            if (s.charAt(i) == LEFT_BRACE)   stack.push(LEFT_BRACE);
+            if (s.charAt(i) == LEFT_BRACKET) stack.push(LEFT_BRACKET);
+
+            if (s.charAt(i) == RIGHT_PAREN) {
+                if (stack.isEmpty())           return false;
+                if (stack.pop() != LEFT_PAREN) return false;
+            }
+
+            else if (s.charAt(i) == RIGHT_BRACE) {
+                if (stack.isEmpty())           return false;
+                if (stack.pop() != LEFT_BRACE) return false;
+            }
+
+            else if (s.charAt(i) == RIGHT_BRACKET) {
+                if (stack.isEmpty())             return false;
+                if (stack.pop() != LEFT_BRACKET) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public static void main(String[] args) {
+        In in = new In();
+        String s = in.readAll().trim();
+        StdOut.println(isBalanced(s));
+    }
+}
