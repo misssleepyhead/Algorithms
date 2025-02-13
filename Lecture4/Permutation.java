527b8fb2965b81a10132fe877331349ebea077e3
misssleepyhead <arielchen8549@gmail.com>
2025-02-13 17:58:55 +0800

    assignments Permutation

--- Lecture4/Permutation.java
+++ Lecture4/Permutation.java
@@ -1,3 +1,24 @@
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
-    public static void main(String[] args){}
+    /**
+     * takes an integer k as a command-line argument;
+     * reads a sequence of strings from standard input using StdIn.readString();
+     * and prints exactly k of them, uniformly at random.
+     * Print each item from the sequence at most once.
+     */
+    public static void main(String[] args) {
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()){
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+        int k = Integer.parseInt(args[0]);
+        if(k> randomizedQueue.size() || k<0){
+            throw new IllegalArgumentException();
+        }
+        for(int i=0;i<k;i++){
+            System.out.println(randomizedQueue.dequeue());
+        }
+
+    }
 }
