201cff35e3542e6552ae4ce42fa4f4552ef3f00c
misssleepyhead <arielchen8549@gmail.com>
2025-02-25 17:56:58 +0800

    MergeX

---
+++ Lecture6/MergeX.java
@@ -0,0 +1,123 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Comparator;
+
+/**
+ * The {@code MergeX} class provides static methods for sorting an array using
+ * an optimized version of mergesort
+ * 1. use insertion sort for small subarrays
+ * 2. Test whether array is already in order
+ * 3. Eliminate the copy to the auxiliary array.
+ */
+
+public class MergeX {
+    private static final int CUTOFF = 7;  // cutoff to insertion sort
+
+    public MergeX() {
+    }
+
+    private static void merge(Comparable[] src, Comparable[] dst, int lo, int mid, int hi) {
+
+        // precondition: src[lo .. mid] and src[mid+1 .. hi] are sorted subarrays
+        assert isSorted(src, lo, mid);
+        assert isSorted(src, mid+1, hi);
+
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)              dst[k] = src[j++];
+            else if (j > hi)               dst[k] = src[i++];
+            else if (less(src[j], src[i])) dst[k] = src[j++];   // to ensure stability
+            else                           dst[k] = src[i++];
+        }
+
+        // postcondition: dst[lo .. hi] is sorted subarray
+        assert isSorted(dst, lo, hi);
+    }
+
+    private static void sort(Comparable[] src, Comparable[] dst, int lo, int hi) {
+        // if (hi <= lo) return;
+        if (hi <= lo + CUTOFF) {
+            insertionSort(dst, lo, hi);
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(dst, src, lo, mid);
+        sort(dst, src, mid+1, hi);
+
+        // if (!less(src[mid+1], src[mid])) {
+        //    for (int i = lo; i <= hi; i++) dst[i] = src[i];
+        //    return;
+        // }
+
+        // using System.arraycopy() is a bit faster than the above loop
+        if (!less(src[mid+1], src[mid])) {
+            System.arraycopy(src, lo, dst, lo, hi - lo + 1);
+            return;
+        }
+
+        merge(src, dst, lo, mid, hi);
+    }
+
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = a.clone();
+        sort(aux, a, 0, a.length - 1);
+
+    }
+
+
+    private static void insertionSort(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    // is a[i] < a[j]?
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    // is a[i] < a[j]?
+    private static boolean less(Object a, Object b, Comparator comparator) {
+        return comparator.compare(a, b) < 0;
+    }
+
+    /***************************************************************************
+     *  Check if array is sorted - useful for debugging.
+     ***************************************************************************/
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    private static boolean isSorted(Object[] a, Comparator comparator) {
+        return isSorted(a, 0, a.length - 1, comparator);
+    }
+
+    private static boolean isSorted(Object[] a, int lo, int hi, Comparator comparator) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1], comparator)) return false;
+        return true;
+    }
+
+    // print array to standard output
+    private static void show(Object[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+}
