148b2a514c6428479505499815dbf6e8727812e3
misssleepyhead <arielchen8549@gmail.com>
2025-03-01 17:29:45 +0800

    QuickSortThreePartitions

---
+++ Lecture6/Lecture7/QuickSortThreePartitions.java
@@ -0,0 +1,92 @@
+package Lecture7;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * The {@code QuickSortThreePartitions} sorting an array
+ * using quick sort with 3-way partitioning
+ */
+public class QuickSortThreePartitions {
+    private static final int CUTOFF = 7;  // cutoff to insertion sort
+
+    public QuickSortThreePartitions() {
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+        assert isSorted(a);
+
+    }
+
+    // use 3-way partitioning quick sort
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int lt = lo, gt = hi;
+        Comparable v = a[lo];
+        int i = lo + 1;
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a, lt++, i++);
+            else if (cmp > 0) {
+                exch(a, i, gt--);
+                i++;
+            } else {
+                i++;
+            }
+        }
+
+        sort(a,lo,lt-1);
+        sort(a,gt+1,hi);
+        assert isSorted(a,lo,hi);
+    }
+
+
+    private static void insertionSort(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+    /***************************************************************************
+     *  Helper sorting functions.
+     ***************************************************************************/
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        if (v == w) return false;   // optimization when reference equals
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+    /***************************************************************************
+     *  Check if array is sorted - useful for debugging.
+     ***************************************************************************/
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+
+    // print array to standard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+}
