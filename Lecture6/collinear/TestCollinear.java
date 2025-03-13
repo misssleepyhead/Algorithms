369f5053b3a38b4d94bbca2c1a972070979c8e57
misssleepyhead <arielchen8549@gmail.com>
2025-03-13 10:52:45 +0800

    Collinears test

---
+++ Lecture6/collinear/TestCollinear.java
@@ -0,0 +1,22 @@
+package collinear;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCollinear {
+
+    @Test
+    public void testNoCollinearPointsBrute(){
+        Point[] points={new Point(100,100),new Point(200,200),new Point(300,500)};
+        BruteCollinearPoints collinearPoints = new BruteCollinearPoints(points);
+        assertEquals(0,collinearPoints.numberOfSegments());
+    }
+
+    @Test
+    public void testNoCollinearPointsFast(){
+        Point[] points={new Point(100,100),new Point(200,200),new Point(300,500)};
+        FastCollinearPoints fp=new FastCollinearPoints(points);
+        assertEquals(0,fp.numberOfSegments());
+    }
+}
