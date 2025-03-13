60dddb85a7e2be04f87172e575c8ed62fb45ee13
misssleepyhead <arielchen8549@gmail.com>
2025-03-13 15:38:04 +0800

    Collinears test

--- Lecture6/collinear/TestCollinear.java
+++ Lecture6/collinear/TestCollinear.java
@@ -3,21 +3,24 @@ package collinear;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-/** Tests for collinear*/
+
+/**
+ * Tests for collinear
+ */
 public class TestCollinear {
 
     @Test
-    public void testNoCollinearPointsBrute(){
-        Point[] points={new Point(100,100),new Point(200,200),new Point(300,500)};
+    public void testNoCollinearPointsBrute() {
+        Point[] points = {new Point(100, 100), new Point(200, 200), new Point(300, 500)};
         BruteCollinearPoints collinearPoints = new BruteCollinearPoints(points);
-        assertEquals(0,collinearPoints.numberOfSegments());
+        assertEquals(0, collinearPoints.numberOfSegments());
     }
 
     @Test
-    public void testNoCollinearPointsFast(){
-        Point[] points={new Point(100,100),new Point(200,200),new Point(300,500)};
-        FastCollinearPoints fp=new FastCollinearPoints(points);
-        assertEquals(0,fp.numberOfSegments());
+    public void testNoCollinearPointsFast() {
+        Point[] points = {new Point(100, 100), new Point(200, 200), new Point(300, 500)};
+        FastCollinearPoints fp = new FastCollinearPoints(points);
+        assertEquals(0, fp.numberOfSegments());
     }
 }
 // Trigger GitHub Actions
