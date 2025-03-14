3c7e40d8f1fbe9375545cbeaa3a4b43bb8827555
misssleepyhead <arielchen8549@gmail.com>
2025-03-14 12:37:12 +0800

    UnorderedArrMaxPQ

---
+++ Lecture89/UnorderedArrMaxPQ.java
@@ -0,0 +1,42 @@
+public class UnorderedArrMaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int n;
+
+    public UnorderedArrMaxPQ(int capacity) {
+        pq = (Key[]) new Comparable[capacity];
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void insert(Key x) {
+        pq[n++] = x;
+    }
+
+    public Key delMax() {
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            if (less(max, i)) max = i;
+        }
+        exch(max, n - 1);
+        return pq[--n];
+    }
+
+
+    // helper functions
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+}
