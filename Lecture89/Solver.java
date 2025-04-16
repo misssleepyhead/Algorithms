942f09e878e23b407806037e9491a84476b221b6
misssleepyhead <arielchen8549@gmail.com>
2025-04-16 19:34:35 +0800

    Slider puzzle solver class

--- Lecture89/Solver.java
+++ Lecture89/Solver.java
@@ -28,25 +28,47 @@ public class Solver {
         }
     }
 
-    private SearchNode searchNode;
+    private SearchNode solutionNode;
     private boolean isSolvable;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null) throw new IllegalArgumentException();
 
+        // add initial node to PQs
         MinPQ<SearchNode> pq = new MinPQ<>();
         MinPQ<SearchNode> twinPQ = new MinPQ<>();
         pq.insert(new SearchNode(initial, 0, null));
         twinPQ.insert(new SearchNode(initial.twin(), 0, null));
+
+
+        while(true){
+            // delete the min from pq
+            SearchNode current= pq.delMin();
+            SearchNode twinCurrent = twinPQ.delMin();
+
+            if(current.board.isGoal()){
+                solutionNode=current;
+                isSolvable=true;
+                break;
+            }else {
+
+            }
+
+
+            // add its neighbors to the pq
+
+        }
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
+        return isSolvable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
+        return isSolvable? solutionNode.moves : -1;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
