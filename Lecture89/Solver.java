fc8ae99bd34473ae8bf7e881775b108e2f105a0a
misssleepyhead <arielchen8549@gmail.com>
2025-04-16 19:01:07 +0800

    Slider puzzle solver class

---
+++ Lecture89/Solver.java
@@ -0,0 +1,60 @@
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.PriorityQueue;
+
+/**
+ * Slider puzzle
+ * implement A* search to solve n by n slider puzzles
+ */
+public class Solver {
+    private static class SearchNode implements Comparable<SearchNode> {
+        private Board board;
+        private int manhattan;
+        private int moves;
+        private int priority;
+        private SearchNode prev;
+
+        public SearchNode(Board board, int moves, SearchNode prev) {
+            this.board = board;
+            this.manhattan = board.manhattan();
+            this.moves = moves;
+            this.priority = this.manhattan + this.moves;
+            this.prev = prev;
+        }
+
+        @Override
+        public int compareTo(SearchNode other) {
+            return Integer.compare(this.priority, other.priority);
+        }
+    }
+
+    private SearchNode searchNode;
+    private boolean isSolvable;
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        if (initial == null) throw new IllegalArgumentException();
+
+        MinPQ<SearchNode> pq = new MinPQ<>();
+        MinPQ<SearchNode> twinPQ = new MinPQ<>();
+        pq.insert(new SearchNode(initial, 0, null));
+        twinPQ.insert(new SearchNode(initial.twin(), 0, null));
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+    }
+
+}
