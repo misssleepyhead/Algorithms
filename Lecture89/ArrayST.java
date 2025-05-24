385abb9ccc8f16ea87a38cbe16d344e9f934d15b
misssleepyhead <arielchen8549@gmail.com>
2025-05-24 18:46:28 +0800

    Exercise 3.1.22

--- Lecture89/ArrayST.java
+++ Lecture89/ArrayST.java
@@ -4,6 +4,11 @@ import edu.princeton.cs.algs4.Queue;
 /**
  * Exercise 3.1.2 :
  * develop ST That uses an unordered array as the underlying data structure
+ *
+ * Exercise 3.1.22: self-organizing search
+ * rearranges items to make those are accessed frequently likely to be found early in the search,
+ * modify search imp to move pair found to the beginning of the list, and the vacated pos to the right one pos
+ * this is called move-to-front heuristic
  */
 
 public class ArrayST<Key, Value> {
@@ -40,7 +45,18 @@ public class ArrayST<Key, Value> {
         if(isEmpty()) return null;
         for(int i=0;i<n;i++){
             if(keys[i].equals(key)){
-                return values[i];
+                // exercises 3.1.22
+                Key tempk = keys[i];
+                Value tempv=values[i];
+
+                // shift everything to the right
+                for(int j=i;j>0;j--){
+                    keys[j]=keys[j-1];
+                    values[j]=values[j-1];
+                }
+                keys[0]=tempk;
+                values[0]=tempv;
+                return values[0];
             }
         }
         return null; // search miss
