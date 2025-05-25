8849e431e9ee809c66c277a0e3e3242f56f558bc
misssleepyhead <arielchen8549@gmail.com>
2025-05-25 15:20:20 +0800

    Exercise 13

--- Lecture89/NonrecursiveBST.java
+++ Lecture89/NonrecursiveBST.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.TreeMap;
 
 /**
@@ -59,15 +62,40 @@ public class NonrecursiveBST<Key extends Comparable<Key>, Value> {
     public Value get(Key key) {
         if (isEmpty()) return null;
         TreeNode x = root;
-        while(x!=null){
-            int cmp=key.compareTo(x.key);
-            if(cmp>0) x=x.right;
-            else if (cmp<0) x=x.left;
-            else{
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp > 0) x = x.right;
+            else if (cmp < 0) x = x.left;
+            else {
                 return x.val;
             }
         }
         return null;
+
+    }
+
+    // inorder traversal of BST to collect keys in ascending order
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new LinkedList<>(); // stores the result(sorted keys)
+        Stack<TreeNode> stack = new Stack<>(); // help simulate recursion
+        TreeNode x = root;
+
+        while (x != null || !stack.isEmpty()) { // continue the loop while there are nodes to process
+            // either going down the tree, or deferred nodes in the stack
+            while (x != null) {
+                stack.push(x); // save current node
+                x = x.left; // inorder : go left
+            }
+            // after hit a null left child, process node and go right
+            // 1. pop the last node
+            x = stack.pop();
+            // 2. store it to queue
+            queue.add(x.key);
+            // 3. explore the right tree
+            x = x.right;
+
+        }
+        return queue;
 
     }
 }
