b5685cc52a4824503af2598672bb7b66aa1d5938
misssleepyhead <arielchen8549@gmail.com>
2025-06-17 19:43:41 +0800

    Assignment 5 , Kdtree

---
+++ Lecture89/KdTree.java
@@ -0,0 +1,9 @@
+/**
+ *  Module 11 assignment: KD-Trees
+ *  2d-tree implementation.
+ *  uses a 2d-tree to implement the same API (but replace PointSET with KdTree).
+ *  A 2d-tree is a generalization of a BST to two-dimensional keys. The idea is to build a BST with points in the nodes,
+ *  using the x- and y-coordinates of the points as keys in strictly alternating sequence.*/
+public class KdTree {
+
+}
