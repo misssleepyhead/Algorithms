d3071c5f2e9008116a62c3a5f8031854040b066c
misssleepyhead <arielchen8549@gmail.com>
2025-06-17 20:02:52 +0800

    Assignment 5 , Kdtree

--- Lecture89/KdTree.java
+++ Lecture89/KdTree.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Point2D;
+
 /**
  *  Module 11 assignment: KD-Trees
  *  2d-tree implementation.
@@ -5,5 +7,18 @@
  *  A 2d-tree is a generalization of a BST to two-dimensional keys. The idea is to build a BST with points in the nodes,
  *  using the x- and y-coordinates of the points as keys in strictly alternating sequence.*/
 public class KdTree {
+
+    private static class PointNode{
+        private final Point2D point; // store x-, y- coordinates as Key
+        private PointNode left;
+        private PointNode right;
+
+        public PointNode(Point2D point) {
+            this.point = point;
+        }
+    }
+    public KdTree(){
+
+    }
 
 }
