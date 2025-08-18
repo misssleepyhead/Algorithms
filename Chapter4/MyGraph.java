270f47bc689495175405cece9f8d4643e1fb888a
Ariel Chen <arielchen8549@gmail.com>
2025-08-18 23:06:10 +0800

    textbook 4.1 practice

--- Chapter4/MyGraph.java
+++ Chapter4/MyGraph.java
@@ -2,40 +2,47 @@ import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 
 /**
- * Class practice 4.1*/
+ * Class practice 4.1
+ */
 public class MyGraph {
     private final int V;
     private int E;
-    private Bag<Integer>[] adj;
+    private Bag<Integer>[] adj; // adjacency lists
 
     public MyGraph(int V) {
         this.V = V;
-        E=0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for(int v=0;v<V;v++){
-            adj[v]=new Bag<Integer>();
+        E = 0;
+        adj = (Bag<Integer>[]) new Bag[V]; // create array of lists
+        for (int v = 0; v < V; v++) { // Initialize all lists to empty
+            adj[v] = new Bag<Integer>();
         }
     }
 
-    public MyGraph(In in){
+    public MyGraph(In in) {
         this(in.readInt());
-        int E=in.readInt();
-        for(int i=0;i<E;i++){
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
             int v = in.readInt();
             int w = in.readInt();
-            addEdge(v,w);
+            addEdge(v, w); // Read vertexes and add edge connecting them
         }
     }
 
-    public int V(){ return V;}
-    public int E(){return E;}
+    public int V() {
+        return V;
+    }
 
-    public void addEdge(int v,int w){
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
         adj[v].add(w);
         adj[w].add(v);
         E++;
     }
-    public Iterable<Integer> adj(int v){
+
+    public Iterable<Integer> adj(int v) {
         return adj[v];
     }
 }
