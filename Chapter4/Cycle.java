c1590f425ca01be03ef6323227227a519be7b589
Ariel Chen <arielchen8549@gmail.com>
2025-08-30 22:28:31 +0800

    textbook 4.1 example Cycle

---
+++ Chapter4/Cycle.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.Graph;
+
+/**
+ * Book example 4.1
+ * solving:
+ * 1. cycle detection: Is the given graph acylic?
+ */
+public class Cycle {
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph g){
+        marked = new boolean[g.V()];
+        for(int s=0;s<g.V();s++){
+            if(!marked[s]){
+                dfs(g,s,s);
+            }
+        }
+    }
+
+    private void dfs(Graph g, int v, int u){
+        marked[v] = true;
+        for(int w:g.adj(v)){
+            if(!marked[w]){
+                dfs(g,w,v);
+            } else if (w!=u) {
+                hasCycle = true;
+            }
+        }
+    }
+    public boolean isHasCycle(){
+        return hasCycle;
+    }
+}
