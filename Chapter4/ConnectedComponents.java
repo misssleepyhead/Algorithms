23a245932ea0161307587be80cee33e97a8e0164
Ariel Chen <arielchen8549@gmail.com>
2025-09-09 23:13:30 +0800

    textbook 4.1 class note

--- Chapter4/ConnectedComponents.java
+++ Chapter4/ConnectedComponents.java
@@ -6,15 +6,15 @@ import edu.princeton.cs.algs4.Graph;
  */
 public class ConnectedComponents {
     private boolean[] marked;
-    private int[] id;
-    private int count;
+    private int[] id; //if[v]=id of component containing v
+    private int count; // number of components
 
     public ConnectedComponents(Graph g) {
         marked = new boolean[g.V()];
         id = new int[g.V()];
         for (int s = 0; s < g.V(); s++) {
             if (!marked[s]) {
-                dfs(g, s);
+                dfs(g, s); // run DFS from one vertex in each component
                 count++;
             }
         }
@@ -22,7 +22,7 @@ public class ConnectedComponents {
 
     private void dfs(Graph g, int v) {
         marked[v] = true;
-        id[v] = count;
+        id[v] = count; // all vertices discovered in same cell of dfs have same id
         for (int w : g.adj(v)) {
             if (!marked[w]) {
                 dfs(g, w);
@@ -32,19 +32,19 @@ public class ConnectedComponents {
 
     /**
      * the vertices in the first component processed, then the value will be 0, meaning they are connected
-      */
+     */
     public boolean connected(int v, int w) {
         return id[v] == id[w];
     }
 
     public int count() {
         return count;
-    }
+    } // number of components
 
     /**
      * component identifier for v
      */
     public int id(int v) {
         return id[v];
-    }
+    } // id of component containing v
 }
