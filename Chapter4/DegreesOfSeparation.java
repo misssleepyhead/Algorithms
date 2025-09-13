0c19321341e04a7b0ff7794aae3748acf8fe8b79
Ariel Chen <arielchen8549@gmail.com>
2025-09-13 20:08:59 +0800

    textbook 4.1 DegreesOfSeparation

---
+++ Chapter4/DegreesOfSeparation.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.BreadthFirstPaths;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.SymbolGraph;
+
+public class DegreesOfSeparation {
+    public static void main(String[] args) {
+        SymbolGraph sg = new SymbolGraph(args[0], args[1]);
+        Graph G = sg.graph();
+        String source = args[2];
+        if (!sg.contains(source)) {
+            System.out.println(source + " not in database");
+            return;
+        }
+
+        int s = sg.index(source);
+        BreadthFirstPaths bfs = new BreadthFirstPaths(G, s);
+        while (!StdIn.isEmpty()) {
+            String sink = StdIn.readLine();
+            if (sg.contains(sink)) {
+                int t = sg.index(sink);
+                if (bfs.hasPathTo(t)) {
+                    for (int v : bfs.pathTo(t)) {
+                        System.out.println(" " + sg.name(v));
+                    }
+                } else {
+                    System.out.println("not connected");
+                }
+            } else {
+                System.out.println("not in database");
+            }
+        }
+    }
+}
