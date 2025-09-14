26ab33defbcfb45db159e5fe0efd0f125bb5da59
Ariel Chen <arielchen8549@gmail.com>
2025-09-14 20:14:01 +0800

    Interview question 2: Diameter

---
+++ Chapter4/Diameter.java
@@ -0,0 +1,51 @@
+import edu.princeton.cs.algs4.BreadthFirstPaths;
+import edu.princeton.cs.algs4.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Interview question 2:
+ * Find Diameter in tree (connected, no cycle graph)
+ */
+public class Diameter {
+    private final int endPointA;
+    private final int endPointB;
+    private List<Integer> path; // vertices on diameter
+    private final int length;   // number of edges on the diameter
+
+
+    public Diameter(Graph G) {
+        // 1. Do the first BFS from an arbitrary vertex to find the farthest A
+        BreadthFirstPaths bfs0 = new BreadthFirstPaths(G, 0);
+        endPointA = farthestVertex(G, bfs0);
+
+        // 2. Do the second bfs from the endpoint A to find the farthest vertex B
+        BreadthFirstPaths bfsA = new BreadthFirstPaths(G, endPointA);
+        endPointB = farthestVertex(G, bfsA);
+
+        // reconstruct diameter path end point a to b from bfsA
+        path = new ArrayList<>();
+        Iterable<Integer> paths = bfsA.pathTo(endPointB);
+        for (int w : paths) {
+            path.add(w);
+        }
+        length = bfsA.distTo(endPointB);
+
+    }
+
+    public int length() {
+        return length;
+    }
+
+    private int farthestVertex(Graph G, BreadthFirstPaths bfs) {
+        int maxDist = -1, endV = -1;
+        for (int v = 0; v < G.V(); v++) {
+            if (bfs.hasPathTo(v) && bfs.distTo(v) > maxDist) {
+                maxDist = bfs.distTo(v);
+                endV = v;
+            }
+        }
+        return endV;
+    }
+}
