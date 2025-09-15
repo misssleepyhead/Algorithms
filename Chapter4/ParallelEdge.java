4a887b3024bd84095d649861e7e654f089c68839
Ariel Chen <arielchen8549@gmail.com>
2025-09-15 22:52:50 +0800

    Creative problem 32,

---
+++ Chapter4/ParallelEdge.java
@@ -0,0 +1,27 @@
+import edu.princeton.cs.algs4.Graph;
+
+/**
+ * Creative problem 32, parallel edge detection: devise a linear time algorithm to count the parallel edges in a graph
+ * parallel edges = multiple edges between the same pair of vertices
+ */
+public class ParallelEdge {
+
+    public int countParallel(Graph g) {
+        int n = g.V();
+        int[] stamp = new int[n];
+        int currentRun = 0;
+        int rawCount = 0;
+
+        for (int v = 0; v < n; v++) {
+            currentRun++; // new stamp for vertex v
+            for (int w : g.adj(v)) {
+                if (stamp[w] == currentRun) { // already met w in this run
+                    rawCount++; // another copy of v-w
+                } else {
+                    stamp[w] = currentRun; // first time we met w
+                }
+            }
+        }
+        return rawCount / 2; // each parallel edge was seen at both ends
+    }
+}
