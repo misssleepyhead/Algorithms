a81d706f1152a7d33e4052bbf7a9b825ce77bd0a
Ariel Chen <arielchen8549@gmail.com>
2025-10-20 22:27:25 +0800

    exercise

---
+++ Chapter4/Degrees.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.Digraph;
+
+/**
+ * 4.2.7
+ * Write a Degrees class to imp following api:
+ * int indegree(int v): indegree of a vertex is the number of directed edges that point to that vertex
+ * int outdegree(int v): outdegree of a vertex is the number of directed edges from that vertex
+ * Iterable<Integer> sources(): a vertex of indegree =0
+ * Iterable<Integer> sinks(): a vertex which outdegree =0
+ * boolean isMap(): a digraph where self-loops allowed and every vertex has outdegree 1 is a map
+ */
+public class Degrees {
+    public Degrees(Digraph G) {
+    }
+
+    public int indegree(int v) {
+    }
+
+    public int outdegree(int v) {
+    }
+
+    public Iterable<Integer> sources() {
+    }
+
+    public Iterable<Integer> sinks() {
+    }
+
+    public boolean isMap() {
+    }
+}
