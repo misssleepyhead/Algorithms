ab094edcdb3451f2b1f349a2c3b2cbd578d03046
Ariel Chen <arielchen8549@gmail.com>
2025-10-24 22:19:44 +0800

    add helper functions

--- Chapter4/MyDirectedDFS.java
+++ Chapter4/MyDirectedDFS.java
@@ -8,20 +8,25 @@ import edu.princeton.cs.algs4.In;
 
 public class MyDirectedDFS {
     private boolean[] marked;
+    private int count;
 
     public MyDirectedDFS(MyDigraph G, int s) {
         marked = new boolean[G.V()];
+        validateVertex(s);
         dfs(G, s);
     }
 
+
     public MyDirectedDFS(MyDigraph G, Iterable<Integer> sources) {
         marked = new boolean[G.V()];
+        validateVertex(sources);
         for (int s : sources) {
             if (!marked[s]) dfs(G, s);
         }
     }
 
     private void dfs(MyDigraph g, int v) {
+        count++;
         marked[v] = true;
         for (int w : g.adj(v)) {
             if (!marked[w]) {
@@ -32,6 +37,27 @@ public class MyDirectedDFS {
 
     public boolean marked(int v) {
         return marked[v];
+    }
+
+    public int count() {
+        return count;
+    }
+
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v > V) throw new IllegalArgumentException();
+
+    }
+
+    private void validateVertex(Iterable<Integer> vertices) {
+        if (vertices == null) throw new IllegalArgumentException();
+        int vertexCount = 0;
+        for (Integer v : vertices) {
+            vertexCount++;
+            if (v == null) throw new IllegalArgumentException();
+            validateVertex(v);
+        }
+        if (vertexCount == 0) throw new IllegalArgumentException();
     }
 
     public static void main(String[] args) {
