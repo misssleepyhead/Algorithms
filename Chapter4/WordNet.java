f1f458402c1deb1aeeb4a6a145e9cf7bb87533db
Ariel Chen <arielchen8549@gmail.com>
2025-10-25 20:28:17 +0800

    WordNet distance() and sap() ok

--- Chapter4/WordNet.java
+++ Chapter4/WordNet.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,12 +12,17 @@ public class WordNet {
     private Map<String, Bag<Integer>> nounID; // a noun might has multiple ids
     private String[] synsetOf; // id and its synset
     private Digraph graph;
+    private final SAP sap;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null) {
             throw new IllegalArgumentException();
         }
+        readSynsetFile(synsets);
+        readHypernyms(hypernyms);
+        rootDAGValidate();
+        sap = new SAP(graph);
 
     }
 
@@ -51,6 +57,7 @@ public class WordNet {
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
+        if (word == null) throw new IllegalArgumentException();
         return nounID.containsKey(word);
     }
 
@@ -69,36 +76,46 @@ public class WordNet {
     }
 
     // the graph must be rooted DAG
-    private void rootDAGValidate(){
+    private void rootDAGValidate() {
         // 1. check if the graph is acyclic (no cycle)
-        if(new DirectedCycle(graph).hasCycle()){
+        if (new DirectedCycle(graph).hasCycle()) {
             throw new IllegalArgumentException("not acyclic");
         }
         // 2. check if there is a root (outdegree =0) and there should be "one" root
-        int root =0;
-        for(int v=0;v<graph.V();v++){
-            if(graph.outdegree(v)==0){
+        int root = 0;
+        for (int v = 0; v < graph.V(); v++) {
+            if (graph.outdegree(v) == 0) {
                 root++;
             }
         }
-        if(root!=1) throw new IllegalArgumentException("not rooted");
+        if (root != 1) throw new IllegalArgumentException("not rooted");
 
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
+        if (nounA == null || nounB == null) throw new IllegalArgumentException();
         if (!isNoun(nounA) || !isNoun(nounB)) {
             throw new IllegalArgumentException();
         }
+        Bag<Integer> idsA = nounID.get(nounA);
+        Bag<Integer> idsB = nounID.get(nounB);
+        return sap.length(idsA, idsB);
 
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
+        if (nounA == null || nounB == null) throw new IllegalArgumentException();
         if (!isNoun(nounA) || !isNoun(nounB)) {
             throw new IllegalArgumentException();
         }
+        Bag<Integer> idsA = nounID.get(nounA);
+        Bag<Integer> idsB = nounID.get(nounB);
+        int ancestorId = sap.ancestor(idsA, idsB);
+
+        return synsetOf[ancestorId];
     }
 
     // do unit testing of this class
