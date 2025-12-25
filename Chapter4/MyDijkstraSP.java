fd27d40d6c9e969d6a48b26d32582d0d1372ce32
Ariel Chen <arielchen8549@gmail.com>
2025-12-25 20:39:38 +0800

    Dijkstra's shortest-paths algorithm

---
+++ Chapter4/MyDijkstraSP.java
@@ -0,0 +1,40 @@
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.EdgeWeightedGraph;
+import edu.princeton.cs.algs4.IndexMinPQ;
+
+/**
+ * 4.4 book imp
+ * Dijkstra's shortest-paths algorithm
+ */
+public class MyDijkstraSP {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
+
+    public MyDijkstraSP(EdgeWeightedDigraph G, int s) {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+        pq = new IndexMinPQ<Double>(G.V());
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        distTo[s] = 0.0;
+        pq.insert(s, 0.0);
+        while (!pq.isEmpty()) {
+            relax(G, pq.delMin());
+        }
+    }
+
+    private void relax(EdgeWeightedDigraph graph, int v) {
+        for (DirectedEdge e : graph.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (pq.contains(w)) pq.changeKey(w, distTo[w]);
+                else pq.insert(w, distTo[w]);
+            }
+        }
+    }
+}
