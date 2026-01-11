303dbdc113130cd18551cc617c96be87b521b8bd
Ariel Chen <arielchen8549@gmail.com>
2026-01-11 21:02:50 +0800

    Arbitrage in current exchange

---
+++ Chapter4/MyArbitrage.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.*;
+
+/**
+ * textbook imp 4.4
+ * Arbitrage in current exchange
+ */
+public class MyArbitrage {
+    public static void main(String[] args) {
+        int V = StdIn.readInt();
+        String[] name = new String[V];
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(V);
+        for (int v = 0; v < V; v++) {
+            name[v] = StdIn.readString();
+            for (int w = 0; w < V; w++) {
+                double rate = StdIn.readDouble();
+                DirectedEdge e = new DirectedEdge(v, w, -Math.log(rate));
+                G.addEdge(e);
+            }
+        }
+        BellmanFordSP spt = new BellmanFordSP(G, 0);
+        if (spt.hasNegativeCycle()) {
+            double stake = 1000.0;
+            for (DirectedEdge e : spt.negativeCycle()) {
+                StdOut.printf("%10.5f %s\n", stake, name[e.to()]);
+                stake *= Math.exp(-e.weight());
+                StdOut.printf("=%10.5f %s\n", stake, name[e.to()]);
+            }
+        } else {
+            StdOut.printf("No arbitrage opportunity ");
+        }
+    }
+}
