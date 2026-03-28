bba684ac195cd96d2207774bb7c825d9ad230b16
Ariel Chen <arielchen8549@gmail.com>
2026-03-28 21:13:49 +0800

    sema carving init the structure

---
+++ Chapter4/SeamCarver.java
@@ -0,0 +1,66 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+    private int width;
+    private int height;
+    private Picture picture;
+
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        if (picture == null) {
+            throw new IllegalArgumentException();
+        }
+        this.picture = new Picture(picture);
+        width = this.picture.width();
+        height = this.picture.height();
+    }
+
+    // current picture
+    public Picture picture() {
+        return new Picture(this.picture);
+    }
+
+    // width of current picture
+    public int width() {
+        return width;
+    }
+
+    // height of current picture
+    public int height() {
+        return height;
+    }
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return 0.0;
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+        if (seam == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+        if (seam == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    //  unit testing (optional)
+    public static void main(String[] args) {
+    }
+
+}
