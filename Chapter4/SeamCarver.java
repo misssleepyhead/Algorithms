1339ffe3278db80fb1589536d97429a5a6dcfd17
Ariel Chen <arielchen8549@gmail.com>
2026-03-29 19:27:25 +0800

    sema carving findseam functions

--- Chapter4/SeamCarver.java
+++ Chapter4/SeamCarver.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.Picture;
 
 import java.awt.*;
 
+
 public class SeamCarver {
     private int width;
     private int height;
@@ -66,13 +67,126 @@ public class SeamCarver {
 
 
     // sequence of indices for horizontal seam
+    // use direct DP: filling the current cell from a previous one
     public int[] findHorizontalSeam() {
-        return null;
+        double[][] distTo = new double[height][width]; // best total energy up to (x,y)
+        int[][] edgeTo = new int[height][width];
+
+        // init first col
+        for (int y = 0; y < height; y++) {
+            distTo[y][0] = energy(0, y);
+            edgeTo[y][0] = -1;
+        }
+
+        // dp from the previous one
+        for (int x = 1; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                distTo[y][x] = Double.POSITIVE_INFINITY;
+
+                for (int prevY = y - 1; prevY <= y + 1; prevY++) {
+                    if (prevY < 0 || prevY >= height) continue;
+
+                    double candidate = distTo[prevY][x - 1] + energy(x, y);
+                    if (candidate < distTo[y][x]) {
+                        distTo[y][x] = candidate;
+                        edgeTo[y][x] = prevY;
+                    }
+                }
+            }
+        }
+
+        // find best end point in last col
+        double minDist = Double.POSITIVE_INFINITY;
+        int minY = -1;
+        for (int y = 0; y < height; y++) {
+            if (distTo[y][width - 1] < minDist) {
+                minDist = distTo[y][width - 1];
+                minY = y;
+            }
+        }
+
+        // reconstruct seam from right to left
+        int[] seam = new int[width];
+        int y = minY;
+        for (int x = width - 1; x >= 0; x--) {
+            seam[x] = y;
+            y = edgeTo[y][x];
+        }
+
+
+        return seam;
     }
 
     // sequence of indices for vertical seam
+    // imp in relax forward style: at the source cell and pushing to the next one
     public int[] findVerticalSeam() {
-        return null;
+        int[] seam = new int[height];
+        double[][] distTo = new double[height][width]; // best total energy up to (x,y)
+        int[][] edgeTo = new int[height][width]; // predecessor column from row y-1
+
+        // init top row, the cost of each pixel in top row is its own energy
+        // if set top row pixel to infinity means they are unreachable which is false
+        for (int x = 0; x < width; x++) {
+            distTo[0][x] = energy(x, 0);
+            edgeTo[0][x] = -1;
+        }
+        for (int y = 1; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                distTo[y][x] = Double.POSITIVE_INFINITY;
+            }
+        }
+        //relax row by row
+        for (int y = 0; y < height - 1; y++) {
+            for (int x = 0; x < width; x++) {
+                // for a vertical seam, we only search down, down left, down right
+                relax(x, y, x, y + 1, distTo, edgeTo); //relax down
+                relax(x, y, x - 1, y + 1, distTo, edgeTo); // down left
+                relax(x, y, x + 1, y + 1, distTo, edgeTo); //down right
+            }
+        }
+
+        // find best endpoint in bottom row
+        double minDist = Double.POSITIVE_INFINITY;
+        int minX = -1;
+        for (int x = 0; x < width; x++) {
+            if (distTo[height - 1][x] < minDist) {
+                minX = x;
+                minDist = distTo[height - 1][x];
+            }
+        }
+
+        // reconstruct seam from bottom to top
+        int x = minX;
+        for (int y = height - 1; y >= 0; y--) {
+            seam[y] = x;
+            x = edgeTo[y][x];
+        }
+
+        return seam;
+    }
+
+    private void relax(int x, int y, int nextx, int nexty, double[][] distTo, int[][] edgeTo) {
+        if (nextx < 0 || nexty < 0 || nextx >= width || nexty >= height) {
+            return;
+        }
+        double candidate = distTo[y][x] + energy(nextx, nexty);
+        if (candidate < distTo[nexty][nextx]) {
+            distTo[nexty][nextx] = candidate;
+            edgeTo[nexty][nextx] = x;
+        }
+    }
+
+    private void relaxHorizontal(int x, int y, int nextx, int nexty,
+                                 double[][] distTo, int[][] edgeTo) {
+        if (nextx < 0 || nextx >= width || nexty < 0 || nexty >= height) {
+            return;
+        }
+
+        double candidate = distTo[y][x] + energy(nextx, nexty);
+        if (candidate < distTo[nexty][nextx]) {
+            distTo[nexty][nextx] = candidate;
+            edgeTo[nexty][nextx] = y;   // store previous ROW
+        }
     }
 
     // remove horizontal seam from current picture
