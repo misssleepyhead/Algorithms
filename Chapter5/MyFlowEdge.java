4973945f2deba135b4a5ef552ded97057bc3487a
Ariel Chen <arielchen8549@gmail.com>
2026-04-25 21:41:48 +0800

    Network-flow algorithm page.897

--- Chapter5/MyFlowEdge.java
+++ Chapter5/MyFlowEdge.java
@@ -1,6 +1,45 @@
+/**
+ * Network-flow algorithm page.897
+ */
 public class MyFlowEdge {
-    public static void main(String[] args) {
-        System.out.println("is");
+    private final int v; // edge source
+    private final int w; // edge target
+    private final double capacity;
+    private double flow;
+
+    public MyFlowEdge(int v, int w, double capacity) {
+        this.v = v;
+        this.w = w;
+        this.capacity = capacity;
+        this.flow = 0.0;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public double capacity() {
+        return capacity;
+    }
+
+    public double flow() {
+        return flow;
+    }
+
+    public double residualCapacityTo(int vertex) {
+        if (vertex == v) return flow;
+        else if (vertex == w) return capacity - flow;
+        else throw new RuntimeException("Inconsistent edge");
+    }
+
+    public void addResidualFlowTo(int vertex, double delta){
+        if(vertex==v) flow-= delta;
+        else if (vertex==w) flow +=delta;
+        else throw new RuntimeException("Inconsistent edge");
     }
 
 }
