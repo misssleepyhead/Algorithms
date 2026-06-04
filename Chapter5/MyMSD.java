58c78a62ab2be5f391c5336d77155948b352b978
Ariel Chen <arielchen8549@gmail.com>
2026-06-04 19:58:09 +0800

    5.2 MSD string sort

--- Chapter5/MyMSD.java
+++ Chapter5/MyMSD.java
@@ -8,10 +8,6 @@ public class MyMSD {
     private static final int M = 15; // cutoff for small subarrays
     private static String[] aux; //auxiliary array for distribution
 
-    private static int charAt(String s, int d) {
-        if (d < s.length()) return s.charAt(d);
-        else return -1; // end of string
-    }
 
     public static void sort(String[] a) {
         int N = a.length;
@@ -19,13 +15,47 @@ public class MyMSD {
         sort(a, 0, N - 1, 0);
     }
 
+    // return dth char of s, -1, if d = length of string
+    private static int charAt(String s, int d) {
+        assert d >= 0 && d <= s.length();
+        if (d == s.length()) return -1;
+        return s.charAt(d);
+    }
+
+    private static void insertion(String[] a, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    // is v less than w, starting at char d
+    private static boolean less(String v, String w, int d) {
+        for (int i = d; i < Math.min(v.length(), w.length()); i++) {
+            if (v.charAt(i) < w.charAt(i)) return true;
+            if (v.charAt(i) > w.charAt(i)) return false;
+        }
+        return v.length() < w.length();
+    }
+
+
     private static void sort(String[] a, int lo, int hi, int d) {
         // sort from a[lo] to a[hi], starting at the dth character
         if (hi <= lo + M) {
-            Insertion.sort(a, lo, hi, d);
+            insertion(a,lo,hi,d);
             return;
         }
-        int[] count = new int[R+2];
+        int[] count = new int[R + 1];
+        int mask = R-1; //0xFF
+
 
     }
 }
